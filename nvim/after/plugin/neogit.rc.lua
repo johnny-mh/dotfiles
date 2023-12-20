2e36b1f50facee6345ba6aaeb889f5de1db942dc
johnny-mh <romz1212@gmail.com>
2023-12-21 00:51:10 +0900

    using lazyvim

--- nvim/after/plugin/neogit.rc.lua
+++
@@ -1,9 +0,0 @@
-local status, neogit = pcall(require, "neogit")
-if (not status) then return end
-
-neogit.setup {}
-
-vim.keymap.set("n", "<leader>g", function()
-  neogit.open()
-end)
-
--- nvim/init.lua
+++ nvim/init.lua
@@ -1,19 +1,2 @@
-require('johnny.base')
-require('johnny.highlights')
-require('johnny.maps')
-require('johnny.plugins')
-
-local has = vim.fn.has
-local is_mac = has "macunix"
-local is_win = has "win32"
-local is_wsl = has "wsl"
-
-if is_mac == 1 then
-  require('johnny.macos')
-end
-if is_win == 1 then
-  require('johnny.windows')
-end
-if is_wsl == 1 then
-  require('johnny.wsl')
-end
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
