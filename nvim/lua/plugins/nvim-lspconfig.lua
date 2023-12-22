00b2596253ffe8cc34323714fedfb3f577d9c2da
johnny-mh <romz1212@gmail.com>
2023-12-22 15:57:59 +0900

    eslint

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,23 +1,8 @@
 return {
   {
     "neovim/nvim-lspconfig",
-    opts = {
-      servers = { eslint = {} },
-      setup = {
-        eslint = function()
-          require("lazyvim.util").lsp.on_attach(function(client)
-            if client.name == "eslint" then
-              client.server_capabilities.documentFormattingProvider = true
-            elseif client.name == "tsserver" then
-              client.server_capabilities.documentFormattingProvider = false
-            end
-          end)
-        end,
-      },
-    },
     init = function()
       local keys = require("lazyvim.plugins.lsp.keymaps").get()
-
       keys[#keys + 1] = { "gr", "<cmd>Telescope lsp_references show_line=false<cr>", desc = "References" }
     end,
   },
