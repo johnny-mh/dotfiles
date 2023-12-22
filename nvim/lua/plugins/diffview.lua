8bf142db5850edadeab668b047ef9cb6d641c20d
johnny-mh <romz1212@gmail.com>
2023-12-22 21:22:19 +0900

    add diffview shortcut

--- nvim/lua/plugins/diffview.lua
+++ nvim/lua/plugins/diffview.lua
@@ -1,3 +1,14 @@
 return {
-  { "sindrets/diffview.nvim" },
+  {
+    "sindrets/diffview.nvim",
+    keys = {
+      { "<leader>gd", "<cmd>DiffviewOpen<cr>", desc = "Open Diffview" },
+      { "<leader>gq", "<cmd>DiffviewClose<cr>", desc = "Close Diffview" },
+    },
+    init = function()
+      require("diffview").setup({
+        enhanced_diff_hl = true,
+      })
+    end,
+  },
 }
