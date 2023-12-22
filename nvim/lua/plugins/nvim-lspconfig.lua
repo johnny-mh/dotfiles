995ff480e793702ec4e246178047c02b93e5b0a2
johnny-mh <romz1212@gmail.com>
2023-12-22 13:02:30 +0900

    lsp reference result pathname fix

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -15,5 +15,10 @@ return {
         end,
       },
     },
+    init = function()
+      local keys = require("lazyvim.plugins.lsp.keymaps").get()
+
+      keys[#keys + 1] = { "gr", "<cmd>Telescope lsp_references show_line=false<cr>", desc = "References" }
+    end,
   },
 }
--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -5,7 +5,15 @@ return {
       "nvim-telescope/telescope-fzf-native.nvim",
       build = "make",
       config = function()
-        require("telescope").load_extension("fzf")
+        local telescope = require("telescope")
+
+        telescope.setup({
+          defaults = {
+            path_display = { "truncate" },
+          },
+        })
+
+        telescope.load_extension("fzf")
       end,
     },
   },
