da0ba9a1cbd3973c672d37026619978e3dd0e8cb
johnny-mh <romz1212@gmail.com>
2024-03-15 09:51:05 +0900

    neovim v0.9.5

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,9 +1,38 @@
 return {
-  {
-    "neovim/nvim-lspconfig",
-    init = function()
-      local keys = require("lazyvim.plugins.lsp.keymaps").get()
-      keys[#keys + 1] = { "gr", "<cmd>Telescope lsp_references show_line=false<cr>", desc = "References" }
-    end,
+  "neovim/nvim-lspconfig",
+  init = function()
+    local keys = require("lazyvim.plugins.lsp.keymaps").get()
+    keys[#keys + 1] = { "gr", "<cmd>Telescope lsp_references show_line=false<cr>", desc = "References" }
+  end,
+  opts = {
+    ---@type lspconfig.options
+    servers = {
+      eslint = {
+        settings = {
+          -- helps eslint find the eslintrc when it's placed in a subfolder instead of the cwd root
+          workingDirectory = { mode = "auto" },
+        },
+      },
+    },
+    setup = {
+      eslint = function()
+        vim.api.nvim_create_autocmd("BufWritePre", {
+          callback = function(event)
+            if not require("lazyvim.util").format.enabled() then
+              -- exit early if autoformat is not enabled
+              return
+            end
+
+            local client = vim.lsp.get_active_clients({ bufnr = event.buf, name = "eslint" })[1]
+            if client then
+              local diag = vim.diagnostic.get(event.buf, { namespace = vim.lsp.diagnostic.get_namespace(client.id) })
+              if #diag > 0 then
+                vim.cmd("EslintFixAll")
+              end
+            end
+          end,
+        })
+      end,
+    },
   },
 }
